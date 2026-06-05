530a9448f1946b4447f4fd0d50f35056317bd805
saranya mariappan <charantechie@gmail.com>
2026-06-05 11:53:03 +0200

    Creating Chat Options and injecting into ChatClient Bean

--- src/main/java/com/ai/openai/advanced/config/ChatClientConfig.java
+++ src/main/java/com/ai/openai/advanced/config/ChatClientConfig.java
@@ -1,7 +1,9 @@
 package com.ai.openai.advanced.config;
 
+import com.ai.openai.advanced.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,10 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        var chatOptionsBuilder = OpenAiChatOptions.builder().model("gpt-5-nano").maxCompletionTokens(600);
         return chatClientBuilder
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultOptions(chatOptionsBuilder)
+                .defaultAdvisors(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor())
                 .defaultSystem("""
                         You are helpful HR assistant who can answer queries on HR policies
                         like leave policy, benefits, employment contracts. 
