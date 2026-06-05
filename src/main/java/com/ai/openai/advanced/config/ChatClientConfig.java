f0d6dbcad0408c5a89246fe6316fc68cd9d43663
saranya mariappan <charantechie@gmail.com>
2026-06-05 09:40:42 +0200

    Default Advisor configuration on the ChatClient level.

--- src/main/java/com/ai/openai/advanced/config/ChatClientConfig.java
+++ src/main/java/com/ai/openai/advanced/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.ai.openai.advanced.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultSystem("""
                         You are helpful HR assistant who can answer queries on HR policies
                         like leave policy, benefits, employment contracts. 
