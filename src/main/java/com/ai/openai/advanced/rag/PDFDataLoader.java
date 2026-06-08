9dc18ac4ee907879f3b9d3efddea8e00b4c4a197
saranya mariappan <charantechie@gmail.com>
2026-06-08 13:47:12 +0200

    Chunking the document before adding to vector store.

--- src/main/java/com/ai/openai/advanced/rag/PDFDataLoader.java
+++ src/main/java/com/ai/openai/advanced/rag/PDFDataLoader.java
@@ -3,6 +3,7 @@ package com.ai.openai.advanced.rag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -26,6 +27,7 @@ public class PDFDataLoader {
     public void loadPDFInToVectorStore(){
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(pdfData);
         List<Document> docs = tikaDocumentReader.get();
-        vectorStore.add(docs);
+        TokenTextSplitter textSplitter  = TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(400).build();
+        vectorStore.add(textSplitter.split(docs));
     }
 }
